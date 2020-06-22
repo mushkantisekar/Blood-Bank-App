f1fbcb09a8db0b6e25272ee507ee3b68ca43169f
mushkantisekar <61748892+mushkantisekar@users.noreply.github.com>
2020-06-23 01:44:40 +0530

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,13 +2,14 @@
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCjFXqLQASDq_nhe-mcpI40iJXJS-7wb-I",
-    authDomain: "blood-bank-abbf8.firebaseapp.com",
-    databaseURL: "https://blood-bank-abbf8.firebaseio.com",
-    projectId: "blood-bank-abbf8",
-    storageBucket: "blood-bank-abbf8.appspot.com",
-    messagingSenderId: "224018960477",
-    appId: "1:224018960477:web:4462e92104505d02"
+    apiKey: "AIzaSyDwW67zcFz6k_YPPwd4WUEB9TnLpCVh0Bo",
+    authDomain: "https://ayesha.firebaseapp.com/",
+    databaseURL: "https://ayesha-786.firebaseio.com/",
+    projectId: "ayesha-786",
+    storageBucket: "ayesha-786.appspot.com",
+    messagingSenderId: "114882299731",
+    appId: "1:114882299731:web:9ac0f7abe2f9dbc8ff83a6"
+    AIzaSyDwW67zcFz6k_YPPwd4WUEB9TnLpCVh0Bo
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
